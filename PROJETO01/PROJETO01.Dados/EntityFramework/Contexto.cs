c2a1a1ba608728a546375f242eb94623dfe5692d
Gabriel Oliveira Borges <040847@campus1.uniaraxa.edu.br>
2021-12-02 21:51:16 -0300

    Party51

---
+++ PROJETO01/PROJETO01.Dados/EntityFramework/Contexto.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using PROJETO01.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace PROJETO01.Dados.EntityFramework
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var conn = @"Data source = 201.62.57.93; 
+                                     Database = GabrielBorges; 
+                                     User ID = RA040847; 
+                                     Password = 040847";
+            optionsBuilder.UseSqlServer(conn);
+        }
+
+        //Nome das Classes que representarão o Banco de Dados
+        public DbSet<Estado> Estado { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Organizador> Organizador { get; set; }
+        public DbSet<CadastroFesta> CadastroFesta { get; set; }
+        public DbSet<Convite> Convite { get; set; }
+
+        //Definição das características das classes com as tabelas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estado>()
+                .ToTable("Estado")
+                .HasKey("UF");
+
+            modelBuilder.Entity<Estado>()
+                .Property("UF")
+                .HasColumnName("Sigla_Est")
+                .HasColumnType("char(2)")
+                .IsRequired();
+
+            modelBuilder.Entity<Estado>()
+                .Property("Nome")
+                .HasColumnName("Nome_Est")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>()
+                .ToTable("Usuario")
+                .HasKey("UsuarioID");
+
+            modelBuilder.Entity<Usuario>()
+                .Property("UsuarioID")
+                .HasColumnName("UsuarioID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Usuario>()
+                .Property("Nome")
+                .HasColumnName("Nome")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("Nome")
+                .HasColumnName("Nome")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+
+
+    }
+}
