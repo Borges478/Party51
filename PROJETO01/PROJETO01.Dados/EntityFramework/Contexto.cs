0b67680b6282db2257e25dd23faef5b7e1e34b2d
Borges478 <69487654+Borges478@users.noreply.github.com>
2021-12-06 16:03:11 -0300

    Party51Atualizado

--- PROJETO01/PROJETO01.Dados/EntityFramework/Contexto.cs
+++ PROJETO01/PROJETO01.Dados/EntityFramework/Contexto.cs
@@ -46,9 +46,9 @@ namespace PROJETO01.Dados.EntityFramework
                 .IsRequired();
 
             base.OnModelCreating(modelBuilder);
-        }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        
+        
+        
             modelBuilder.Entity<Usuario>()
                 .ToTable("Usuario")
                 .HasKey("UsuarioID");
@@ -64,13 +64,165 @@ namespace PROJETO01.Dados.EntityFramework
                 .HasColumnName("Nome")
                 .HasColumnType("varchar(100)")
                 .IsRequired();
+            
             modelBuilder.Entity<Usuario>()
+                .Property("DataNascimento")
+                .HasColumnName("DataNascimento")
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+            
+            modelBuilder.Entity<Usuario>()
+                .Property("Telefone")
+                .HasColumnName("Telefone")
+                .HasColumnType("varchar(11)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("Email")
+                .HasColumnName("Email")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("Endereco")
+                .HasColumnName("Endereco")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("CPF")
+                .HasColumnName("CPF")
+                .HasColumnType("char(11)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("Bairro")
+                .HasColumnName("Bairro")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("Sexo")
+                .HasColumnName("Sexo")
+                .HasColumnType("int")
+                .IsRequired();
+            modelBuilder.Entity<Usuario>()
+                .Property("CEP")
+                .HasColumnName("CEP")
+                .HasColumnType("char(9)")
+                .IsRequired();
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Organizador>()
+                    .ToTable("Organizador")
+                    .HasKey("OrganizadorID");
+
+            modelBuilder.Entity<Organizador>()
+                .Property("OrganizadorID")
+                .HasColumnName("OrganizadorID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Organizador>()
                 .Property("Nome")
                 .HasColumnName("Nome")
                 .HasColumnType("varchar(100)")
                 .IsRequired();
 
+            modelBuilder.Entity<Organizador>()
+                .Property("CPF")
+                .HasColumnName("CPF")
+                .HasColumnType("char(11)")
+                .IsRequired();
+
+            modelBuilder.Entity<Organizador>()
+                .Property("Telefone")
+                .HasColumnName("Telefone")
+                .HasColumnType("varchar(11)")
+                .IsRequired();
+            
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CadastroFesta>()
+                .ToTable("CadastroFesta")
+                .HasKey("FestaID");
+
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("FestaID")
+                .HasColumnName("FestaID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("Nome")
+                .HasColumnName("Nome")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("Endereco")
+                .HasColumnName("Endereco")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("Estado")
+                .HasColumnName("Estado")
+                .HasColumnType("char(2)")
+                .IsRequired();
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("DataFesta")
+                .HasColumnName("DateFesta")
+                .HasColumnType("Date")
+                .IsRequired();
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("Bairro")
+                .HasColumnName("Bairro")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("PrecoIngresso")
+                .HasColumnName("PrecoIngresso")
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            modelBuilder.Entity<CadastroFesta>()
+                .Property("OrganizadorID")
+                .HasColumnName("OrganizadorID")
+                .HasColumnType("int")
+                .IsRequired();
+            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Convite>()
+                .ToTable("Convite")
+                .HasKey("ConviteID");
+
+            modelBuilder.Entity<Convite>()
+                .Property("ConviteID")
+                .HasColumnName("ConviteID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Convite>()
+                .Property("UsuarioID")
+                .HasColumnName("UsuarioID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Convite>()
+                .Property("OrganizadorID")
+                .HasColumnName("OrganizadorID")
+                .HasColumnType("int")
+                .IsRequired();
+
+            modelBuilder.Entity<Convite>()
+                .Property("FestaID")
+                .HasColumnName("FestaID")
+                .HasColumnType("int")
+                .IsRequired();
+            
+            base.OnModelCreating(modelBuilder);
+
+
+
+
+
+
         }
 
 
